48c6df3baf1032c25701c97caa05d788bf5735a1
Noppanut <Noptechnology@gmail.com>
2024-05-17 11:36:38 +0700

    example4-v6

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -102,6 +102,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     "a Latin professor at Hampden-Sydney College in Virginia, "
                     "looked up one of the more obscure Latin words, consectetur, "
                     "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
+                ,textAlign: TextAlign.justify,
               ),
             ),
           ],
