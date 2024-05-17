8763e582ff56cee47b4c740c3138f8a543339a47
Noppanut <Noptechnology@gmail.com>
2024-05-17 11:32:01 +0700

    example4-v5

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -75,31 +75,34 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 40,
             ),
-            Text(
-              "Hello Contrary to popular belief,"
-                  "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
-                  "making it over 2000 years old. Richard McClintock, "
-                  "a Latin professor at Hampden-Sydney College in Virginia, "
-                  "looked up one of the more obscure Latin words, consectetur, "
-                  "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
-                  "Hello Contrary to popular belief,"
-                  "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
-                  "making it over 2000 years old. Richard McClintock, "
-                  "a Latin professor at Hampden-Sydney College in Virginia, "
-                  "looked up one of the more obscure Latin words, consectetur, "
-                  "from a Lorem Ipsum passage, and going through the cites of the word in classical literature,"
-                  "Hello Contrary to popular belief,"
-                  "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
-                  "making it over 2000 years old. Richard McClintock, "
-                  "a Latin professor at Hampden-Sydney College in Virginia, "
-                  "looked up one of the more obscure Latin words, consectetur, "
-                  "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
-                  "Hello Contrary to popular belief,"
-                  "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
-                  "making it over 2000 years old. Richard McClintock, "
-                  "a Latin professor at Hampden-Sydney College in Virginia, "
-                  "looked up one of the more obscure Latin words, consectetur, "
-                  "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                "Hello Contrary to popular belief,"
+                    "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
+                    "making it over 2000 years old. Richard McClintock, "
+                    "a Latin professor at Hampden-Sydney College in Virginia, "
+                    "looked up one of the more obscure Latin words, consectetur, "
+                    "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
+                    "Hello Contrary to popular belief,"
+                    "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
+                    "making it over 2000 years old. Richard McClintock, "
+                    "a Latin professor at Hampden-Sydney College in Virginia, "
+                    "looked up one of the more obscure Latin words, consectetur, "
+                    "from a Lorem Ipsum passage, and going through the cites of the word in classical literature,"
+                    "Hello Contrary to popular belief,"
+                    "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
+                    "making it over 2000 years old. Richard McClintock, "
+                    "a Latin professor at Hampden-Sydney College in Virginia, "
+                    "looked up one of the more obscure Latin words, consectetur, "
+                    "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
+                    "Hello Contrary to popular belief,"
+                    "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, "
+                    "making it over 2000 years old. Richard McClintock, "
+                    "a Latin professor at Hampden-Sydney College in Virginia, "
+                    "looked up one of the more obscure Latin words, consectetur, "
+                    "from a Lorem Ipsum passage, and going through the cites of the word in classical literature, "
+              ),
             ),
           ],
         ),
